10d2d5571c49eb456a60b1951efa773398ee5f8e
Vitalii Kovalchuk <vitaliikovalchuk55@gmail.com>
2021-04-21 20:58:20 +0300

    creating-first-components

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="res in storeResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+
+export default {
+  components: {
+    LearningResource,
+  },
+
+  data() {
+    return {
+      storeResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Gide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'TLearn to google...',
+          link: 'https://google.org',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
