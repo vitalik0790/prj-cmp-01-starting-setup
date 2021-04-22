fde20a9461edd7899bad44bea813a2d1cbe2a3bc
Vitalii Kovalchuk <vitaliikovalchuk55@gmail.com>
2021-04-22 15:53:04 +0300

    add-buttons

--- src/components/learning-resources/LearningResource.vue
+++ src/components/learning-resources/LearningResource.vue
@@ -3,12 +3,12 @@
     <base-card>
       <header>
         <h3>{{ title }}</h3>
-        <button>Delete</button>
+        <base-button mode="flat"></base-button>
       </header>
-    <p>{{ description }}</p>
-    <nav>
-      <a :href="link">View Resource</a>
-    </nav>
+      <p>{{ description }}</p>
+      <nav>
+        <a :href="link">View Resource</a>
+      </nav>
     </base-card>
   </li>
 </template>
