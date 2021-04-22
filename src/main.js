96bfdd8c9a65c781330ba4577c4ff90508f24466
Vitalii Kovalchuk <vitaliikovalchuk55@gmail.com>
2021-04-22 12:25:34 +0300

    TheHeader

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue'
+import BaseCard from './components/UI/BaseCard'
 
 const app = createApp(App);
+app.component('base-card', BaseCard)
+
 app.mount('#app');
